f1c86694a9288ff2a7b87de19ce56846d05acaed
SlavomirMazurPantheon <67378888+SlavomirMazurPantheon@users.noreply.github.com>
2022-01-25 14:33:26 +0100

    Move API overview into separate component (#41)
    - Text and examples updates
    - Moved into separate component
    
    Signed-off-by: Slavomir Mazur <slavomir.mazur@pantheon.tech>

---
+++ src/app/features/yang-validator/api-overview/api-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'yc-api-overview',
+  templateUrl: './api-overview.component.html',
+  styleUrls: ['./api-overview.component.scss']
+})
+export class ApiOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
