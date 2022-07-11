f86322fe0331a7568a432f6baed93d83bea366d2
SlavomirMazurPantheon <67378888+SlavomirMazurPantheon@users.noreply.github.com>
2022-07-11 18:05:14 +0200

    fix: yangvalidator API docs component displaying (#95)
    Signed-off-by: Slavomir Mazur <slavomir.mazur@pantheon.tech>

--- src/app/features/yang-validator/yang-validator-routing.module.ts
+++ src/app/features/yang-validator/yang-validator-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
     {
         path: '',
         component: YangValidatorComponent
+    },
+    {
+        path: ':validating',
+        component: YangValidatorComponent
     }
 ];
 
