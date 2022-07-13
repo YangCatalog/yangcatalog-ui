c1d47c2041fa3adcc976de2bc0d0cd32ff447cb7
SlavomirMazurPantheon <67378888+SlavomirMazurPantheon@users.noreply.github.com>
2022-07-13 08:42:28 +0200

    docs: added link to the swagger documentation (#96)
    Signed-off-by: Slavomir Mazur <slavomir.mazur@pantheon.tech>

--- src/app/features/yang-validator/api-overview/api-overview.component.ts
+++ src/app/features/yang-validator/api-overview/api-overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'yc-api-overview',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./api-overview.component.scss']
 })
 export class ApiOverviewComponent implements OnInit {
+
+  myBaseUrl = environment.WEBROOT_BASE_URL;
 
   constructor() { }
 
